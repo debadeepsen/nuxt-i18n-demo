4368deb4d9c37992b9705bfd516c8d073bb0586a
debadeep.MSI <debadeep@MSI>
2022-01-03 20:07:21 +0530

    initial commit

---
+++ message.config.js
@@ -0,0 +1,34 @@
+const messages = {
+    en: {
+        greeting: 'Hello, my name is Debadeep and I am a software engineer',
+        subheading: 'Welcome to my portfolio',
+        footer: 'Full list on my website',
+        projects: [
+            {
+                title: 'US Election Results Simulator',
+                link: 'https://usaelectionsim.softwarecradle.com/'
+            },
+            {
+                title: 'A word guessing game',
+                link: 'https://wordgame.softwarecradle.com/'
+            }
+        ]
+    },
+    fr: {
+        greeting: 'Salut, je m\'appelle Debadeep et je suis ingénieur logiciel',
+        subheading: 'Bienvenue dans mon portfolio',
+        footer: 'Liste complète sur mon site',
+        projects: [
+            {
+                title: 'Simulateur des résultats des élections américaines',
+                link: 'https://usaelectionsim.softwarecradle.com/'
+            },
+            {
+                title: 'Un jeu pour deviner des mots',
+                link: 'https://wordgame.softwarecradle.com/'
+            }
+        ]
+    }
+}
+
+export default messages;
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="locale-changer">
+      <button :class="getButtonClass('en')" @click="changeLocale('en')">
+        EN
+      </button>
+      <button :class="getButtonClass('fr')" @click="changeLocale('fr')">
+        FR
+      </button>
+    </div>
+    <h1>{{ $t("greeting") }}</h1>
+    <h3>{{ $t("subheading") }}</h3>
+    <ul>
+      <li v-for="(p, i) in $t('projects')" :key="i">
+        {{ p.title }} -
+        <a target="_blank" :href="p.link">{{ p.link }}</a>
+      </li>
+    </ul>
+    <hr />
+    <div>
+      {{ $t("footer") }} -
+      <a target="_blank" href="https://www.debadeepsen.com"
+        >https://www.debadeepsen.com</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    changeLocale(locale) {
+      this.$i18n.locale = locale;
+    },
+
+    getButtonClass(locale) {
+      if (this.$i18n.locale === locale) {
+        return "locale-selected";
+      }
+      return "locale-unselected";
+    },
+  },
+};
+</script>
+
